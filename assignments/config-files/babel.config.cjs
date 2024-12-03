6e5869e1fddad059763383bc547e8e2dea772d9b
EsenKaratas <esennurkaratass@gmail.com>
2024-12-03 14:19:30 +0100

    Revert "node.js assignment w2"
    This reverts commit fde7ee9ff59091ee1fb9e9841b46b31dcc7ade8f.

--- assignments/config-files/babel.config.cjs
+++ assignments/config-files/babel.config.cjs
@@ -1,6 +1,7 @@
 module.exports = {
   presets: [
     [
+      // This is a configuration, here we are telling babel what configuration to use
       "@babel/preset-env",
       {
         targets: {
--- assignments/hackyourtemperature/sources/keys.js
+++
@@ -1,5 +0,0 @@
-const keys = {
-  API_KEY: "12224101017bdb7e7e92088d5a2188b3",
-};
-
-export default keys;
